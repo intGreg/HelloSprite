c328abaa0905160f0773c388b6f49717001d7878
intGreg <internationalgreg@gmail.com>
2013-11-29 19:10:49 +0100

    Update PYMyScene.m
    add soccer field in background

--- HelloSprite/PYMyScene.m
+++ HelloSprite/PYMyScene.m
@@ -9,18 +9,21 @@
 
 -(id)initWithSize:(CGSize)size {    
     if (self = [super initWithSize:size]) {
-        /* Setup your scene here */
         
-        self.backgroundColor = [SKColor colorWithRed:0.15 green:0.15 blue:0.3 alpha:1.0];
+        // Set background image
+        SKSpriteNode *snField = [SKSpriteNode spriteNodeWithImageNamed:@"SoccerField.gif"];
+        snField.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame));
+        snField.size = CGSizeMake(400,240);
+        snField.name = @"BACKGROUND";
+        [self addChild:snField];
         
-        SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
-        
-        myLabel.text = @"Hello, World!";
-        myLabel.fontSize = 30;
-        myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-                                       CGRectGetMidY(self.frame));
-        
-        [self addChild:myLabel];
+        // Create the sprite
+        if (!self.sprite) {
+            self.sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
+            self.sprite.size = CGSizeMake(15, 15);
+            self.sprite.position = CGPointMake(0, 0);
+            [self addChild:self.sprite];
+        }
     }
     return self;
 }
